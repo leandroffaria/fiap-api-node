f29db5c742ba6f0d46d3ec4f76cbe60128e7c705
Leandro Faria <leandro.faria@mercadolivre.com>
2019-10-08 22:32:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('MEU AMBIENTE', process.env.NODE_ENV);
+const express = require('express');
+
+const app = express();
+
+app.get('/users', (request, response, next) => {
+    console.log("ENTREI NA ROTA USERS");
+    response.json({ success: true });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
